88f2f3e32e8c0d964c13436b00907159de448968
1628158677 <1628158677@qq.com>
2025-05-21 23:50:38 +0800

    一次修改

---
+++ Assets/readBytesTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Table;
+using UnityEngine;
+
+public class readBytesTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<weapon> weapons = weapon.LoadBytes();
+        for (int i = 0; i < weapons.Count; i++)
+        {
+            weapon w = weapons[i];
+            string log = w.id + "," + w.name + "," + w.prefabName + ",";
+            for (int d = 0; d < w.desc.Count; d++)
+            {
+                log += w.desc[d] + ",";
+            }
+            for (int n = 0; n < w.desc.Count; n++)
+            {
+                log += w.desc[n] + ",";
+            }
+            Debug.Log(log);
+        }
+    }
+}
